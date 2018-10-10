f9ab57736dbae67fa3d966ad90dc9660f7b1cf5e
LoicBurnotte <37924969+LoicBurnotte@users.noreply.github.com>
2018-10-10 15:54:12 +0200

    Upload project withour node_module
    1) Make "npm install" command for adding Vendors. 
    2) "ng serve"

---
+++ src/app/app.module.ts
@@ -0,0 +1,68 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from './components/app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CallbackComponent } from './components/callback/callback.component';
+import { OAuthInterceptor } from './interceptors/oauth-interceptor';
+import { AuthService } from './services/auth.service';
+import { NavComponent } from './components/nav/nav.component';
+import { SpotifyService } from './services/spotify.service';
+import { TrackComponent } from './components/track/track.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { AlbumComponent } from './components/album/album.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'callback',
+    component: CallbackComponent
+  },
+  {
+    path: 'artist',
+    component: ArtistComponent
+  },
+  {
+    path: 'track',
+    component: TrackComponent
+  },
+  {
+    path: 'album',
+    component: AlbumComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CallbackComponent,
+    NavComponent,
+    TrackComponent,
+    ArtistComponent,
+    AlbumComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: OAuthInterceptor,
+    multi: true,
+  },
+    AuthService, 
+    SpotifyService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
